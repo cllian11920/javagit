1c5bc91fc83b5b2d9227a3b7222c3e47f8c7d564
cllian11920 <176911158@qq.com>
2019-09-18 22:40:13 +0800

    confict resolved

--- javagit2/src/javagit2/Test2.java
+++ javagit2/src/javagit2/Test2.java
@@ -4,8 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("java git2");
-		System.out.println("java git4");
 		System.out.println("java git3");
+		System.out.println("java git4");
 	}
 
 }
